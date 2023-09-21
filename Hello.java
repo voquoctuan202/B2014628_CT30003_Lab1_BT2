e92b91c355bd88b2e371669de251f813163b6349
voquoctuan202 <voquoctuan425@gmail.com>
2023-09-21 16:05:03 +0700

    Insert \n behind 'Hello World'

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String []arg){
-        System.out.print("Hello World");
-    
+        System.out.print("Hello World\n");
+        System.out.print("Insert \\n behind \"Hello World\"");
     }
 }
